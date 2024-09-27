3749e697e3f77a24e22d89d5b4580cdbf5150c59
Michael Robbeloth <michael@robbeloth.com>
2024-09-26 23:34:12 -0400

    Add docs to count example

--- examples/count.s
+++ examples/count.s
@@ -9,15 +9,22 @@
 .text
 
 _start:
+    # load beginning value
     la x5, beg
     lw x28, 0(x5)
+    # load ending value
     la x6, end
     lw x29, 0(x6)
     
+        # add one to start
 loop:   addi x28, x28, 1
+        # move/copy inc val to first function argument
         mv x10, x28
+        # call function in libcsc3054 static library
         jal x1, printNumLineFeed
+        # if beg value equals ending value, end program
         bge x28, x29, terminate
+        # else jump back to beginning of loop
         jal x0, loop
     
     # exit the program
